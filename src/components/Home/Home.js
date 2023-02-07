70435a29431609406471489e92554dcb779f1876
Atikur Rahaman <atikurahad@gmail.com>
2023-02-07 23:46:38 +0600

    banner added successfully

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,23 @@ import './home.css'
 
 const Home = () => {
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet porro reprehenderit nisi ullam accusantium ducimus voluptates? At eligendi possimus illo esse modi, fugit placeat fuga debitis assumenda error vel repellat.</p>
-      <div className='image-bg'>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi saepe eius ea.</p>
+    <>
+      <div className="bg-black">
+        <div className=" w-[fit-content] grid sm:grid-cols-2 items-center	">
+          <div className="image-bg"></div>
+          <div className='text-center'>
+            {" "}
+            <p className=" text-center my-auto font-serif  mx-auto text-5xl p-3 text-emerald-500	">
+              Online shopping is a form of electronic commerce which allows
+              consumers <br /> to directly buy goods. <br />
+            </p>
+            <button className="text-black m-3 font-bold text-xl mx-auto bg-yellow-400 p-2 px-3 rounded-full hover:bg-slate-600 hover:text-white">
+              BUY NOW
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const Product = (props) => {
-  const { name, img } = props.product;
+  const { name, img, category, seller, stock, ratings } = props.product;
   return (
     <>
-      <div className="mx-auto text-center border rounded-sm border-gray-500	">
+      <div className="mx-auto text-center border rounded-sm border-gray-500	bg-[#3af1b8]">
         <img className="h-96 w-96" src={img} alt="productImg" />
+        <p>Catergory: {category}</p>
         <h1 className="text-1xl text-bold"> Name: {name}</h1>
+        <p>Seller: {seller}</p>
+        <p>Stock:{stock}</p>
+        <p>Rating {ratings}</p>
       </div>
     </>
   );
